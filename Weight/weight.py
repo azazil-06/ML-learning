dc7cdceeaa82f3cf7cbe5047429e993b41592570
azazil-06 <arjunprasadsa@gmail.com>
2025-08-07 14:41:05 +0530

    new

--- Weight/weight.py
+++ Weight/weight.py
@@ -6,8 +6,13 @@ dataset = pd.read_csv("weight_predict_real.csv")
 x=dataset[["height","age","bmi","muscle_mass","body_fat"]]       
 y=dataset[["weight"]] 
 
+getHeight = int(input("Enter height:"))
+getAge = int(input("Enter age:"))
+getBMI = int(input("Enter BMI:"))
+getMuscleMass = int(input("Enter musclemass:"))
+getbodyfat = int(input("Enter bodyfat %:"))
 
 model=lm.LinearRegression()
 model.fit(x,y)  
 
-print(model.predict([[160,20,80,10,9]]))
+print(model.predict([[getHeight,getAge,getBMI,getMuscleMass,getbodyfat]]))
