b68c06ff9ecad2dcb0168331802be5e2b479f6ac
azazil-06 <arjunprasadsa@gmail.com>
2025-08-07 14:02:37 +0530

    ll

---
+++ Nonnumerical/Bigsalary/bigsalaryknn.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import sklearn.neighbors as knn
+from sklearn.preprocessing import LabelEncoder
+
+mydata = pd.read_csv("big_salary_data.csv")
+
+le = LabelEncoder()
+mydata["education_qualification_encoded"] = le.fit_transform(mydata[["education_qualification"]]) #x inte string value encoded into number
+
+x = mydata[["education_qualification_encoded"]]
+y = mydata[["salary"]]
+
+model = knn.KNeighborsRegressor(n_neighbors=3)
+model.fit(x,y)                 
+
+print(model.predict([[160]])) 
+
+print(mydata)
